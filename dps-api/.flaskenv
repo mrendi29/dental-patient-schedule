955f964353488cb6f7a47ebafb9a0d55c44ee6af
Endi Caushi <mrmendi29@gmail.com>
2022-02-04 21:27:40 -0500

    Setup V1

---
+++ dps-api/.flaskenv
@@ -0,0 +1,1 @@
+_APP=wsgi.py
---
+++ dps-api/dps_api/db/model.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import current_app
+
+db = SQLAlchemy()
+
+
+class Dentist(db.Model):
+    pass
+
+
+class Patients:
+    pass
+
+
+class Records:
+    pass
