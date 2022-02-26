535a1e3b062fd0e55d32a938fdb77bc012ad0d35
Endi Caushi <mrmendi29@gmail.com>
2022-02-25 23:25:42 -0500

    heroku default dialect

--- dps-api/wsgi.py
+++ dps-api/wsgi.py
@@ -1,10 +1,12 @@
 from dps_api import create_app
 import os
 
+db_url = os.getenv("DATABASE_URL").replace("://", "ql://", 1) or os.getenv(
+    "SQLALCHEMY_DATABASE_URI"
+)
 app = create_app(
     config={
-        "SQLALCHEMY_DATABASE_URI": os.getenv("DATABASE_URL")
-        or os.getenv("SQLALCHEMY_DATABASE_URI"),
+        "SQLALCHEMY_DATABASE_URI": db_url,
         "SQLALCHEMY_TRACK_MODIFICATIONS": False,
     }
 )
